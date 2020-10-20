76afe73d946b6ac8c39207b623ca7d9aa2e96123
davbyr <dbyrne@noc.ac.uk>
2020-10-20 15:34:12 +0100

    Changes to get build_xios_archer working

--- nemo/nemo_build_utils.sh
+++ nemo/nemo_build_utils.sh
@@ -17,9 +17,7 @@ load_modules_archer() {
 
 fancy_print(){
 	# More noticeable printing to stdout
-	echo "  ***"
 	echo "  | >>>>>>>>>>>> $1"
-	echo "  ***"
 }
 
 build_xios_archer() {
@@ -28,34 +26,27 @@ build_xios_archer() {
 	export ARCH_DIR=$2
 	
 	# Determine if desired output dir exists. If not, create it.
-	if [! -d "$XIOS_DIR" ]; then
+	if [ ! -d "$XIOS_DIR" ]; then
 		mkdir "$XIOS_DIR"
 	fi
 	
+        load_modules_archer
+
 	fancy_print "Downloading and compiling XIOS in $XIOS_DIR"
 
-	cd $XIOS_DIR # Previous DIR = $PWD
-	
 	# Checkout XIOS version
-	fancy_print " Checkout out XIOS repository"
-	svn co http://forge.ipsl.jussieu.fr/ioserver/svn/XIOS/branchs/xios-2.5@1627 xios
+	fancy_print " Checkout XIOS repository"
+	svn co http://forge.ipsl.jussieu.fr/ioserver/svn/XIOS/branchs/xios-2.5@1627 $XIOS_DIR
 	
 	# Get arch files and move them across to the fresh checkout
-	cp $ARCH_DIR/* ./nemo/arch
-	cd xios
+	cp $ARCH_DIR/* $XIOS_DIR/arch
+	cd $XIOS_DIR
 	
 	# Compile XIOS
 	fancy_print " Compiling XIOS"
 	./make_xios --full --prod --arch archer --netcdf_lib netcdf4_par --job 4
 }
 
-build_nemo_archer() {
-
-}
-
-recompile_nemo_archer(){
-
-}
 
 # This thing allows for the above functions to be called externally
 "$@"
